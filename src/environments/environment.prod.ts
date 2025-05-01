39ee355a781b4fb42fa2f0b70fb9b85b7565bd5d
brkzmn <burakozmen06@gmai.com>
2025-05-01 16:46:54 +0200

    add environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://clinicaltrials.gov/api/v2/studies',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://clinicaltrials.gov/api/v2/studies',
+};
